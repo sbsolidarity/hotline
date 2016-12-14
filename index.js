7cad38a90d55476b6a443b8e4cc27131d883fbee
Alex Dunn <dunn.alex@gmail.com>
2016-12-13 20:15:50 -0800

    Local functionality (no Sheets/Drive integration)

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 ////
 
 const PORT = 3030
-const SETTINGS = require('./config/twilio.json')
+const TWILIO = require('./config/twilio.json')
 // handlers
 const handle = require('./lib/handlers.js')
 
 const express = require('express')
 const app = express()
+// http://expressjs.com/en/4x/api.html#req.body
 const bodyParser = require('body-parser')
 const multer = require('multer')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-app.post(SETTINGS.callback_url, multer().array(), handle.twilio)
+app.post(TWILIO.sms_callback, multer().array(), handle.sms)
+app.post(TWILIO.voice_callback, multer().array(), handle.voice)
+app.post(TWILIO.audio_callback, multer().array(), handle.audio)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
