5fd3a5a940b707d1d5a65f759a038b64c6becbec
Alex Dunn <dunn.alex@gmail.com>
2017-04-29 21:39:11 -0700

    voice message: use recorded response rather than text-to-speech

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,8 @@ set :repo_url, ENV.fetch('REPO', 'https://gitlab.com/welp/solidarity.git')
 set :branch, ENV.fetch('BRANCH', 'master')
 
 set :deploy_to, '/var/www'
+
+set :linked_dirs, %w[public/audio]
 set :linked_files,
     %w(
       config/application.json
